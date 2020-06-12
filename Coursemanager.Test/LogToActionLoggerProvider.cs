bc76baa7a33f4d6aa01222596bcaebb7a33a7d84
Robert T. Ogden <robert.t.ogden@aexp.com>
2020-06-12 12:57:52 -0700

    initial commit

---
+++ Coursemanager.Test/LogToActionLoggerProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coursemanager.Test
+{
+    internal class LogToActionLoggerProvider : ILoggerProvider
+    {
+        private readonly Action<string> _efCoreLogAction;
+        private readonly LogLevel _logLevel;
+
+       
+
+        public LogToActionLoggerProvider(Action<string> efCoreLogAction, LogLevel logLevel = LogLevel.Information)
+        {
+            _efCoreLogAction = efCoreLogAction;
+            _logLevel = logLevel;
+        }
+
+
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new EfCoreLogger(_efCoreLogAction, _logLevel);
+        }
+
+        public void Dispose()
+        {
+           // nothing to dispose
+        }
+    }
+}
